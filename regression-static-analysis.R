f478bb1453449a95a312d305be56579d3dd6deed
mattjhayes <matthew_john_hayes@hotmail.com>
2015-10-19 22:46:31 +1300

    update from mattjhayes

--- regression-static-analysis.R
+++ regression-static-analysis.R
@@ -28,36 +28,6 @@ if (length(files_dir_2) < 1) {
   stop("No subdirectories?")
 }
 
-# ===================== CHART FUNCTIONS ================================
-fx_chart_scatter_1 <- function(data_x, data_y, data_type, data.frame, chart_title, x_axis_label, y_axis_label) {
-    # Scatter lattice with panel per test type and R squared stat analysis:
-    # Build formula from variables we were passed:
-    y_vs_x <- paste(data_y, data_x, sep="~")
-    chart_formula <- formula(paste(y_vs_x, data_type, sep="|"))
-    scatter.lattice <- xyplot(chart_formula, 
-                          data = data.frame,
-                          main=chart_title,
-                          panel = function(x, y, ...) {
-                            panel.xyplot(x, y, ...)
-                            lm1 <- lm(y ~ x)
-                            lm1sum <- summary(lm1)
-                            r2 <- lm1sum$adj.r.squared
-                            panel.abline(a = lm1$coefficients[1], 
-                                         b = lm1$coefficients[2])
-                            panel.text(labels = 
-                                         bquote(italic(R)^2 == 
-                                                  .(format(r2, 
-                                                           digits = 3))),
-                                       x = 30, y = 1000)
-                            },
-                          xscale.components = xscale.components.subticks,
-                          yscale.components = yscale.components.subticks,
-                          xlab=x_axis_label,
-                          ylab=y_axis_label,
-                          as.table = TRUE)
-    p = scatter.lattice
-    print (p)
-}
 # =============================== BUILD FILE DATA FUNCTION =============
 fx_build_file_data <- function(file_name, files_dir_2) {
     # Use this to build file data prior to importing CSVs
@@ -90,59 +60,30 @@ fx_build_file_data <- function(file_name, files_dir_2) {
     return(returnList)
 }
 
-# ============================= CSVs to DATA FRAME =====================
-fx_csv2df <- function(files_list, files) {
-    # Passed a list of CSV file data frames, files information, specific
-    # columns of interest and their final names and return a single 
-    # data frame that incorporates all this information
-    df_result <- data.frame()
-    # iterate through files and add to result data frame
-    for (i in 1:length(files_list)) {
-        test_type <- unname(files$test_types[i])
-        dir_path <- unname(files$dir_path[i])
-        x <- files_list[[i]]$time
-        df_tmp <- data.frame(x)
-        colnames(df_tmp)[1] = "Time"
-        # Add in the 'y' column(s):
-        j <- 2
-        for (col_y in col_select) {
-            df_tmp$col_y <- files_list[[i]][,col_y]
-            colnames(df_tmp)[j] <- col_names[j - 1]
-            j <- j + 1
-        }
-        #*** Add filled Test_Type column:
-        df_tmp$Test_Type <- as.factor(rep(test_type, length(x)))
-        #*** Add filled Dir_Path column:
-        df_tmp$Dir_Path <- as.factor(rep(dir_path, length(x)))
-        # Accumulate the additional data rows:
-        df_result = rbind(df_result, df_tmp)
-    }
-    # Set Time column to POSIXct data type:
-    df_result$Time <- as.POSIXct(df_result$Time)
-    return(df_result)
-}
-
 # ===================== MAIN PROGRAM ===================================
 # Note: Iperf CSV format is:
-# Time, local_IP, local_port, server_IP, server_port, duration, Interval, Transfer, Bandwidth 
+# Time, local_IP, local_port, server_IP, server_port, duration,
+#   Interval, Transfer, Bandwidth 
 
 # Call function (see further up) to build file data:
-files <- fx_build_file_data("pc1.example.com-iperf_result.txt", files_dir_2)
-    
+files_1234 <- fx_build_file_data("pc1.example.com-1234-iperf_result.txt", files_dir_2)
+files_5555 <- fx_build_file_data("pc1.example.com-5555-iperf_result.txt", files_dir_2)
+
 print ("Reading Iperf result CSV files into a list")
 # Read the result CSV files into a list:
-files_list <- lapply(files$files, read.csv, header=FALSE)
+files_list_1234 <- lapply(files_1234$files, read.csv, header=FALSE)
+files_list_5555 <- lapply(files_5555$files, read.csv, header=FALSE)
 
-# Produce a data frame of just what we need in right format:
-df_iperf <- data.frame()
-for (i in 1:length(files_list)) {
-    test_type <- unname(files$test_types[i])
-    dir_path <- unname(files$dir_path[i])
+# Produce a data frame of just what we need in right format for tcp-1234:
+df_iperf_1234 <- data.frame()
+for (i in 1:length(files_list_1234)) {
+    test_type <- unname(files_1234$test_types[i])
+    dir_path <- unname(files_1234$dir_path[i])
     df_tmp <- data.frame(i)
     colnames(df_tmp)[1] = "Test Number"
     # Add in the 'y' column(s):
-    df_tmp[2] <- files_list[[i]][,8]
-    df_tmp[3] <- files_list[[i]][,9]
+    df_tmp[2] <- files_list_1234[[i]][,8]
+    df_tmp[3] <- files_list_1234[[i]][,9]
     colnames(df_tmp)[2] <- "Transfer"
     colnames(df_tmp)[3] <- "Bandwidth"
     #*** Add filled Test_Type column:
@@ -150,13 +91,50 @@ for (i in 1:length(files_list)) {
     #*** Add filled Dir_Path column:
     df_tmp$Dir_Path <- dir_path
     # Accumulate the additional data rows:
-    df_iperf = rbind(df_iperf, df_tmp)
+    df_iperf_1234 = rbind(df_iperf_1234, df_tmp)
 }
+
+# Produce a data frame of just what we need in right format for tcp-5555:
+df_iperf_5555 <- data.frame()
+for (i in 1:length(files_list_5555)) {
+    test_type <- unname(files_5555$test_types[i])
+    dir_path <- unname(files_5555$dir_path[i])
+    df_tmp <- data.frame(i)
+    colnames(df_tmp)[1] = "Test Number"
+    # Add in the 'y' column(s):
+    df_tmp[2] <- files_list_5555[[i]][,8]
+    df_tmp[3] <- files_list_5555[[i]][,9]
+    colnames(df_tmp)[2] <- "Transfer"
+    colnames(df_tmp)[3] <- "Bandwidth"
+    #*** Add filled Test_Type column:
+    df_tmp$Test_Type <- test_type
+    #*** Add filled Dir_Path column:
+    df_tmp$Dir_Path <- dir_path
+    # Accumulate the additional data rows:
+    df_iperf_5555 = rbind(df_iperf_5555, df_tmp)
+}
+
+# Produce a merged data frame
+df_combined <-merge(df_iperf_1234, df_iperf_5555, all=T, by="Test Number")
+colnames(df_combined)[names(df_combined)=="Transfer.x"] <- "Transfer TCP-1234"
+colnames(df_combined)[names(df_combined)=="Bandwidth.x"] <- "Bandwidth TCP-1234"
+colnames(df_combined)[names(df_combined)=="Transfer.y"] <- "Transfer TCP-5555"
+colnames(df_combined)[names(df_combined)=="Bandwidth.y"] <- "Bandwidth TCP-5555"
+colnames(df_combined)[names(df_combined)=="Test_Type.x"] <- "Test_Type"
+colnames(df_combined)[names(df_combined)=="Dir_Path.x"] <- "Dir_Path"
+drops <- c("Test_Type.y","Dir_Path.y")
+df_combined <- df_combined[,!(names(df_combined) %in% drops)]
 
 # ============================= CHARTING ===============================
 # Plot results on a chart:
-g <- ggplot(df_iperf, aes(x=Test_Type, y=Bandwidth)) +
-    geom_point(shape=1) + scale_y_log10() + ggtitle("Static Regression") +
+g <- ggplot(df_iperf_1234, aes(x=Test_Type, y=Bandwidth)) +
+    geom_point(shape=1) + scale_y_log10() + ggtitle("Static Regression TCP-1234") +
+    xlab("Test Type") +
+    ylab("Bandwidth (bps - Log10)")
+print (g)
+
+g <- ggplot(df_iperf_5555, aes(x=Test_Type, y=Bandwidth)) +
+    geom_point(shape=1) + scale_y_log10() + ggtitle("Static Regression TCP-5555") +
     xlab("Test Type") +
     ylab("Bandwidth (bps - Log10)")
 print (g)
