5be4ded1f3dd2bac3eb909bcb864f4c81e5219e1
mattjhayes <matthew_john_hayes@hotmail.com>
2016-04-16 13:32:43 +1200

    update from mattjhayes

--- tc_timely_data_pre_processing.py
+++ tc_timely_data_pre_processing.py
@@ -20,24 +20,30 @@ ingestion by R
 Pass it full path to directory that contains test result
 files to pre-process
 
+Example:
+
+./tc_timely_data_pre_processing.py ~/results/timeliness/statistical \
+     /20160414211230/nmeta2-constrained-bw-iperf/20160414211435
+
 """
 
 import os, sys
 
-import time
+#*** Time-related imports:
 from datetime import datetime
-from datetime import timedelta
 import pytz
 from tzlocal import get_localzone
 
+#*** Regular Expressions import:
 import re
 
+#*** Constants for filenames to process:
 FILENAME_IPERF_START = 'iperf_starttime.txt'
-FILENAME_FLOWUPDATES = 'sw1.example.com-OF-snooping.txt' 
+FILENAME_FLOWUPDATES = 'sw1.example.com-OF-snooping.txt'
 
-treatment_time = 0
-
-utc = pytz.utc
+#*** Timezones (for conversions to UTC):
+UTC = pytz.utc
+LOCAL_TZ = get_localzone()
 
 #*** Must have 1 parameter passed to it (first parameter is script)
 assert len(sys.argv) == 2
@@ -45,54 +51,70 @@ assert len(sys.argv) == 2
 #*** Get parameters from command line
 TEST_DIR = sys.argv[1]
 
-#*** Read in the Iperf start time:
-FILENAME = os.path.join(TEST_DIR, FILENAME_IPERF_START)
+def check_snoop_line(snoop_line, timezone):
+    """
+    Passed a line from the OVS snooping file and determine if it is
+    the treatment being applied. If treatment found, return the
+    time in usable format (datetime object with correct timezone).
+    """
+    #*** Extract date/time from the line:
+    #*** Example line start: 2016-04-14 09:14:38.592: OFPT_FLOW_MOD...
+    #*** date time is in group 1:
+    of_snoop_match = \
+                re.match(r"^(\d+\-\d+\-\d+\s+\d+\:\d+\:\d+\.\d+).*",
+                snoop_line)
+    if of_snoop_match:
+        #*** Now see if line contains pattern for a treatment:
+        treatment_match = \
+                re.search(r"actions\=set_queue\:1\,goto\_table\:5",
+                snoop_line)
+        if treatment_match:
+            print "matched treatment on line ", snoop_line
+            tt_datetime = of_snoop_match.group(1)
+            pattern = '%Y-%m-%d %H:%M:%S.%f'
+            tt_datetime2 = datetime.strptime(tt_datetime, pattern)
+            tt_datetime2_tz = timezone.localize(tt_datetime2)
+            print "treatment_time_tz is ", tt_datetime2_tz
+            return tt_datetime2_tz
+    return 0
 
-with open(FILENAME, 'r') as f:
-    IPERF_STARTTIME = f.readline()
+if __name__ == '__main__':
+    #=========== MAIN PROGRAM =====================
+    treatment_time = 0
 
-print "IPERF_STARTTIME is", IPERF_STARTTIME
-iperf_starttime_datetime = datetime.fromtimestamp(float(IPERF_STARTTIME))
-print "iperf_starttime_datetime is ", iperf_starttime_datetime
+    #*** Read in the Iperf start time:
+    FILENAME = os.path.join(TEST_DIR, FILENAME_IPERF_START)
+    with open(FILENAME, 'r') as f:
+        IPERF_STARTTIME = f.readline()
+    iperf_starttime = datetime.fromtimestamp(float(IPERF_STARTTIME))
+    iperf_starttime_datetime_tz = LOCAL_TZ.localize(iperf_starttime)
+    print "Iperf was started at", iperf_starttime_datetime_tz
 
-# get local timezone    
-local_tz = get_localzone() 
-print "local_tz =", local_tz
+    #*** Read in the TC Flow Entry (Treatment) Install Time on Switch:
+    FILENAME = os.path.join(TEST_DIR, FILENAME_FLOWUPDATES)
+    with open(FILENAME, 'r') as f:
+        for line in f.readlines():
+            if not treatment_time:
+                #*** Call function to check the line to see if it is
+                #***  a treatment and if so return time in UTC timezone:
+                treatment_time = check_snoop_line(line, UTC)
 
-iperf_starttime_datetime_tz = local_tz.localize(iperf_starttime_datetime)
-print "iperf_starttime_datetime_tz is ", iperf_starttime_datetime_tz
+    #*** Calculate the delta between Iperf start and traffic treatment:
+    if iperf_starttime_datetime_tz and treatment_time:
+        delta = treatment_time - iperf_starttime_datetime_tz
+        print "delta seconds is ", delta.total_seconds()
+        #*** Write result to file:
 
-#*** Read in the TC Flow Entry Install Time:
-FILENAME = os.path.join(TEST_DIR, FILENAME_FLOWUPDATES)
-
-with open(FILENAME, 'r') as f:
-    for line in f.readlines():
-        #*** date time is in group 1:
-        of_snoop_match = re.match("^(\d+\-\d+\-\d+\s+\d+\:\d+\:\d+\.\d+).*", line)
-        if of_snoop_match:
-            print "Matched date time ", of_snoop_match.group(1)
-            treatment_match = re.search("actions\=set_queue\:1\,goto\_table\:5", line)
-            if treatment_match:
-                print "matched treatment on line ", line
-                if not treatment_time:
-                    treatment_time = of_snoop_match.group(1)
-                    print "treatment_time is ", treatment_time
-                    pattern = '%Y-%m-%d %H:%M:%S.%f'
-                    treatment_time1 = datetime.strptime(treatment_time, pattern)
-                    print "treatment_time1 is ", treatment_time1
-                    treatment_time_tz = utc.localize(treatment_time1)
-                    print "treatment_time_tz is ", treatment_time_tz 
-
-#*** Non-DST dependant...
-delta = treatment_time_tz - iperf_starttime_datetime_tz 
-print "delta is ", delta
-
-print "delta seconds is ", delta.total_seconds()
+        # TBD
 
 
-#2016-04-13 08:29:16.591: OFPT_FLOW_MOD (OF1.3) (xid=0xcd67b0fb): ADD table:4 priority=1,tcp,nw_src=10.1.0.1,nw_dst=10.1.0.2,tp_src=35898,tp_dst=5555 idle:30 out_port:0 actions=set_queue:1,goto_table:5
-    
-#https://pymotw.com/2/re/
+    else:
+        #*** Uh-oh, something must have gone wrong... Lets write
+        #*** something to file for triage later:
 
-#print "IPERF_STARTTIME is", IPERF_STARTTIME
+        # TBD
+
+        sys.exit("Uh-oh, something must have gone wrong...")
+
+
 
